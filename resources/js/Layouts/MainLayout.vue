f548a2b3a0a2ca7820c6d70ae7a909467aaed8c7
Mythrand <jakub.kajanovic1@gmail.com>
2024-06-06 19:49:04 +0200

    First commit - page

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import {ref} from 'vue'
+
+const timer = ref(0)
+setInterval(() => timer.value++, 1000)
+</script>
+<template>
+    <div>
+      <header>
+        <nav>
+          <!-- Navigation Links -->
+          <Link href="/">Main Page</Link>
+          <Link href="/hello">Show Page</Link>
+          <div>The page with time {{ timer }}</div>
+        </nav>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+      <footer>
+        <p>© 2024 Your Company</p>
+      </footer>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MainLayout'
+  }
+  </script>
+  
+  <style scoped>
+
+  header {
+    background: #333;
+    color: white;
+    padding: 1rem;
+  }
+  nav a {
+    color: white;
+    margin-right: 1rem;
+  }
+  footer {
+    background: #333;
+    color: white;
+    text-align: center;
+    padding: 1rem;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+  }
+  </style>
+  
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,7 @@
+<template>
+    
+      <h1>Home Page</h1>
+      <p>Welcome to the home page</p>
+    
+  </template>
+  
